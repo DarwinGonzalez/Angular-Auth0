4ed63765cddbe473f5186b3adca2fb3bac6edfe6
DarwinGonzalez <alu0100812794@ull.edu.es>
2019-02-28 10:33:40 +0100

    Finish implementation of basic auth0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { ProtectedComponent } from './components/protected/protected.component';
 
 import { APP_ROUTING } from './app.routes';
 import { AuthService } from './services/auth.service';
+import { AuthGuardService } from './services/auth-guard.service';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,10 @@ import { AuthService } from './services/auth.service';
     BrowserModule,
     APP_ROUTING
   ],
-  providers: [AuthService],
+  providers: [
+    AuthService,
+    AuthGuardService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
